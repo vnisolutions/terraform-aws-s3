bec619ad25ce38766a4dbb6f969c6fa0137a46f7
lamvini <lamvini.hcmus@gmail.com>
2023-01-04 18:50:11 +0700

    format code

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 resource "aws_s3_bucket" "s3-bucket" {
-    bucket = "${var.env}-${var.project_name}-bucket"
+  bucket = "${var.env}-${var.project_name}-bucket"
 
   tags = {
-      Name  = "${var.env}-${var.project_name}-bucket"
-      Environment = "${var.env}"
-      Management  = "terraform"
+    Name        = "${var.env}-${var.project_name}-bucket"
+    Environment = "${var.env}"
+    Management  = "terraform"
   }
 
 }
